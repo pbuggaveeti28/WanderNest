9f4dae44d217b5db0bd20b1760230576200de7f4
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-03-23 16:40:01 -0700

    Created navbar and hero section

---
+++ my-app/src/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.png";
+function Navbar() {
+  return (
+    <navbar>
+      <img className="navbar-logo" src={logo} alt="" />
+    </navbar>
+  );
+}
+export default Navbar;
